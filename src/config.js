9bdd0c060bdf510ef87ae7d878af2c1e1836ea65
jmt9816 <jordantlr4@gmail.com>
2024-02-12 11:28:36 -0700

    Exclusion updates

---
+++ src/config.js
@@ -0,0 +1,123 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.sol;
+
+// General metadata 
+const namePrefix = "n00bs";
+const description =
+  "n00bs bring the iconic heroes and lore from ReBoot Worlds into diversely curated, pixelated PFPs.";
+
+const baseUri = "onlyusethisforethereum";
+
+const solanaMetadata = {
+  symbol: "n00b",
+  seller_fee_basis_points: 600, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://rebootworlds.com/",
+  creators: [
+    {
+      address: "4wFD3TewF28RjZNpVk6T1pXWufLpTUMnAJN2ZXwKC8uf",
+      share: 100,
+    },
+  ],
+};
+
+// layer order matters, first layer is in the back, last later is in the front (on "top")
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 5000,
+    layersOrder: [
+      { name: "Sky" },
+      { name: "Background" },
+      { name: "Species" },
+      { name: "Expression" },
+      { name: "Clothing" },
+      { name: "Head" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1024,
+  height: 1024,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
---
+++ src/exclusionConfig.js
@@ -0,0 +1,260 @@
+const exclusionRules = [
+  {
+    Species: ["Dark Human"],
+    Expression: ["Light Angel", "Light Angry", "Light Black", "Light Devil", "Light Happy",
+      "Light Stoned", "Light Surprised", "Light White", "Orc Angel", "Orc Angry", "Orc Black",
+      "Orc Devil", "Orc Happy", "Orc Stoned", "Orc Surprised", "Orc White", "Simian Angel",
+      "Simian Angry", "Simian Black", "Simian Devil", "Simian Happy", "Simian Stoned", "Simian Surprised",
+      "Simian White", "Undead Angel", "Undead Angry", "Undead Black", "Undead Devil", "Undead Happy",
+      "Undead Stoned", "Undead Surprised", "Undead White"],
+  },
+  {
+    Species: ["Light Human"],
+    Expression: ["Dark Angel", "Dark Angry", "Dark Black", "Dark Devil", "Dark Happy", "Dark Stoned",
+      "Dark Surprised", "Dark White", "Orc Angel", "Orc Angry", "Orc Black", "Orc Devil", "Orc Happy",
+      "Orc Stoned", "Orc Surprised", "Orc White", "Simian Angel", "Simian Angry", "Simian Black",
+      "Simian Devil", "Simian Happy", "Simian Stoned", "Simian Surprised", "Simian White", "Undead Angel",
+      "Undead Angry", "Undead Black", "Undead Devil", "Undead Happy", "Undead Stoned", "Undead Surprised", "Undead White"],
+  },
+  {
+    Species: ["Orc"],
+    Expression: ["Dark Angel", "Dark Angry", "Dark Black", "Dark Devil", "Dark Happy", "Dark Stoned",
+      "Dark Surprised", "Dark White", "Light Angel", "Light Angry", "Light Black", "Light Devil", "Light Happy",
+      "Light Stoned", "Light Surprised", "Light White", "Simian Angel", "Simian Angry", "Simian Black",
+      "Simian Devil", "Simian Happy", "Simian Stoned", "Simian Surprised", "Simian White", "Undead Angel",
+      "Undead Angry", "Undead Black", "Undead Devil", "Undead Happy", "Undead Stoned", "Undead Surprised", "Undead White"],
+  },
+  {
+    Species: ["Simian"],
+    Expression: ["Dark Angel", "Dark Angry", "Dark Black", "Dark Devil", "Dark Happy", "Dark Stoned",
+      "Dark Surprised", "Dark White", "Light Angel", "Light Angry", "Light Black", "Light Devil", "Light Happy",
+      "Light Stoned", "Light Surprised", "Light White", "Orc Angel", "Orc Angry", "Orc Black", "Orc Devil",
+      "Orc Happy", "Orc Stoned", "Orc Surprised", "Orc White", "Undead Angel", "Undead Angry", "Undead Black",
+      "Undead Devil", "Undead Happy", "Undead Stoned", "Undead Surprised", "Undead White"],
+  },
+  {
+    Species: ["Undead Elf"],
+    Expression: ["Dark Angel", "Dark Angry", "Dark Black", "Dark Devil", "Dark Happy", "Dark Stoned",
+      "Dark Surprised", "Dark White", "Light Angel", "Light Angry", "Light Black", "Light Devil", "Light Happy",
+      "Light Stoned", "Light Surprised", "Light White", "Orc Angel", "Orc Angry", "Orc Black", "Orc Devil",
+      "Orc Happy", "Orc Stoned", "Orc Surprised", "Orc White", "Simian Angel", "Simian Angry", "Simian Black",
+      "Simian Devil", "Simian Happy", "Simian Stoned", "Simian Surprised", "Simian White"],
+  },
+  {
+    Species: ["Orc"],
+    Background: ["Ethereal Forest"],
+  },
+  {
+    Clothing: ["Heart"],
+    "Body Accent": ["Beer Rings", "Wine Stains"],
+  },
+  {
+    Background: ["ReBoot Theme Park"],
+    Clothing: ["Denim Jacket"],
+  },
+  {
+    Background: ["ReBoot Theme Park"],
+    Clothing: ["Highborn Knight Armor"],
+  },
+  {
+    Background: ["Ethereal Forest"],
+    Clothing: ["Florida Man"],
+  },
+  {
+    Background: ["Ethereal Forest"],
+    Clothing: ["Toly Onesie"],
+  },
+  {
+    Background: ["Ethereal Forest"],
+    Clothing: ["Forest Camo ACU"],
+  },
+  {
+    Background: ["Necrothorns Abyss"],
+    Head: ["Valkyrie"],
+  },
+  {
+    Sky: ["Blue"],
+    Clothing: ["Highborn Knight Armor"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Clothing: ["Spartan ODU"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Head: ["Gray Cowboy Hat"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Clothing: ["Playmaker Onesie"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Head: ["Oni Mask"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Head: ["Kangol"],
+  },
+  {
+    Sky: ["Gloomy"],
+    Species: ["Undead Elf"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Clothing: ["Desert Camo ACU"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Clothing: ["Clayno Onesie"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Head: ["Golden Mane"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Head: ["Sombrero"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Head: ["Blonde"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Head: ["Chartist"],
+  },
+  {
+    Sky: ["Sandstorm"],
+    Head: ["Cowboy Hat"],
+  },
+  {
+    Sky: ["Purple Haze"],
+    Clothing: ["Fosace Robe"],
+  },
+  {
+    Sky: ["Solana"],
+    Clothing: ["Fosace Robe"],
+  },
+  {
+    Clothing: ["Fosace Robe"],
+    Head: ["Golden Mane"],
+  },
+  {
+    Head: ["Gangster Hat"],
+    Species: ["Undead Elf"],
+  },
+  {
+    Head: ["Chartist"],
+    Species: ["Simian"],
+  },
+  {
+    Head: ["Chartist"],
+    Species: ["Orc"],
+  },
+  {
+    Head: ["Chartist"],
+    Species: ["Undead Elf"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Dark Stoned"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Dark Black"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Light Stoned"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Light Black"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Simian Black"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Undead Black"],
+  },
+  {
+    Head: ["Ski Mask"],
+    Expression: ["Orc Angel"],
+  },
+  {
+    Head: ["Forest Camo Helmet"],
+    Clothing: ["Highborn Knight Armor"],
+  },
+  {
+    Clothing: ["Clayno Onesie"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["Necromancer"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["Playmakers Onesie"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["ReBoot Hoodie"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["Rogue Archer"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["SMB Onesie"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Clothing: ["Toly Onesie"],
+    Head: ["Barbarian Helmet", "BK Hat", "Black Bandana", "Blonde", "Brunet", "Buccaneer Cap", "Bucket Hat",
+      "Chartist", "Clean Fade", "Cowboy Hat", "Dreadlocks", "First Knight Helm", "Forest Camo Helmet", "Gangster Hat",
+      "Golden Mane", "Gun Top", "Hadoken", "Hipster Blue", "Hipster Red", "Kangol", "Legion Helmet", "Mage Hat", "Man Bun",
+      "Newsboy", "Oni Mask", "ReBoot Trucker", "Sailor Hat", "Ski Mask", "Solana Cap", "Sombrero",
+      "Tactical Helmet", "The King", "Undercut", "Valkyrie", "Vipers", "White Bandana", "ZAMN", "Gray Cowboy Hat",],
+  },
+  {
+    Head: ["No Hat"],
+    Clothing: ["Artist Overalls", "Barbarian Armor", "BK Shirt", "Black Suit", "Brown Suit",
+     "Buccaneer Coat", "Bulletproof Vest", "Cloud", "Critters Jersey", "Cult Shirt",
+      "Cyberpunk Jacket", "Denim Jacket", "Desert Camo ACU", "First Knight", "Florida Man",
+      "Forest Camo ACU", "Fosace Robe", "Goron Tunic", "Highborn Knight Armor", "Hypebeast Shirt",
+      "Legion Breastplate", "Mage Cloak", "Mando Armor", "MF Jersey", "Navy Suit",
+      "Nuke Dukem", "ReBoot Aviator", "ReBoot Jockey Jacket",
+      "Royal Officer", "Sailor", "Samurai", "Solana Jersey", "Spartan ODU", "Steampunk Armor",
+      "Trench Coat", "Tribal Poncho", "Wan Obi Cloak",]
+  },
+];
+
+module.exports = exclusionRules;
